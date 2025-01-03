db953e67367d2cd03db7d985cec3b6b2fe545653
danrae pray <danrae.pray@outlook.com>
2025-01-02 23:58:13 -0600

    built a grid renderer!

---
+++ src/GridRenderer.kt
@@ -0,0 +1,73 @@
+package com.wizneylabs.leetcoding
+
+class GridRenderer(rows: Int, cols: Int) {
+
+    companion object {
+
+        val RESET = "\u001B[0m" // resets terminal styles
+
+        // Basic background colors
+        val BG_BLACK = "\u001B[40m"
+        val BG_RED = "\u001B[41m"
+        val BG_GREEN = "\u001B[42m"
+        val BG_YELLOW = "\u001B[43m"
+        val BG_BLUE = "\u001B[44m"
+        val BG_MAGENTA = "\u001B[45m"
+        val BG_CYAN = "\u001B[46m"
+        val BG_WHITE = "\u001B[47m"
+
+        // Bright/light background colors
+        val BG_BRIGHT_BLACK = "\u001B[100m"   // Gray
+        val BG_BRIGHT_RED = "\u001B[101m"
+        val BG_BRIGHT_GREEN = "\u001B[102m"
+        val BG_BRIGHT_YELLOW = "\u001B[103m"
+        val BG_BRIGHT_BLUE = "\u001B[104m"
+        val BG_BRIGHT_MAGENTA = "\u001B[105m"
+        val BG_BRIGHT_CYAN = "\u001B[106m"
+        val BG_BRIGHT_WHITE = "\u001B[107m"
+    }
+
+    private val _gridData : Array<Array<String>> =
+        Array(rows) { Array(cols) { BG_BRIGHT_BLACK } };
+
+    private val _rows = rows;
+    private val _cols = cols;
+
+    fun draw() {
+
+        // top row
+        for (i in 0..this._rows + 1)
+        {
+            print("${BG_BLACK}   ${RESET}");
+        }
+
+        println("");
+
+        for (i in 0..this._rows - 1)
+        {
+            print("${BG_BLACK}   ${RESET}");
+
+            for (j in 0..this._cols - 1)
+            {
+                val cellColor = this._gridData[i][j];
+
+                print("${cellColor}   ${RESET}");
+            }
+
+            print("${BG_BLACK}   ${RESET}");
+            println("");
+        }
+
+        // bottom row
+        for (i in 0..this._rows + 1)
+        {
+            print("${BG_BLACK}   ${RESET}");
+        }
+    }
+
+    fun setCellColor(row: Int, col: Int, color: String) {
+
+        this._gridData[row][col] = color;
+    }
+
+}
--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,4 @@
+import com.wizneylabs.leetcoding.GridRenderer
 import com.wizneylabs.leetcoding.exercises.HashTableLeetcode
 import com.wizneylabs.leetcoding.MaxHeap
 
@@ -8,7 +9,7 @@ fun testRemove(heap : MaxHeap) {
     println("max value = ${maxValue}");
 }
 
-fun main() {
+fun maxHeapTest() {
 
     val heap = MaxHeap();
 
@@ -26,4 +27,34 @@ fun main() {
     testRemove(heap);
 
     heap.print();
+}
+
+fun testGridRenderer() {
+
+    val renderer = GridRenderer(15, 15);
+
+    for (i in 1..15 - 2) {
+
+        renderer.setCellColor(1, i, GridRenderer.BG_WHITE);
+    }
+
+    for (i in 2..15 - 2) {
+
+        renderer.setCellColor(i, 13, GridRenderer.BG_WHITE);
+    }
+
+    renderer.setCellColor(0, 0, GridRenderer.BG_WHITE);
+    renderer.setCellColor(0, 1, GridRenderer.BG_WHITE);
+    renderer.setCellColor(1, 0, GridRenderer.BG_WHITE);
+
+    renderer.setCellColor(13, 14, GridRenderer.BG_WHITE);
+    renderer.setCellColor(14, 14, GridRenderer.BG_WHITE);
+    renderer.setCellColor(14, 13, GridRenderer.BG_WHITE);
+
+    renderer.draw();
+}
+
+fun main() {
+
+    testGridRenderer();
 }
