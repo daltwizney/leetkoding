2089f76fa2457b8b3fd56a1bedcd288b12e2e618
danrae pray <danrae.pray@outlook.com>
2025-01-16 22:37:56 -0600

    i still got it o/

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,7 @@
 import com.wizneylabs.leetcoding.GridRenderer
 import com.wizneylabs.leetcoding.exercises.HashTableLeetcode
 import com.wizneylabs.leetcoding.MaxHeap
+import java.util.*
 import kotlin.math.max
 import kotlin.math.min
 
@@ -11,24 +12,107 @@ class TreeNode(var `val` : Int) {
 
 class Solution {
 
-    fun _maxDepthRecursive(root: TreeNode?): Int {
+    fun computeMaxDepth(root: TreeNode?): Int {
 
         if (root == null)
         {
             return 0;
         }
 
-        val leftChildMaxDepth = _maxDepthRecursive(root.left);
-        val rightChildMaxDepth = _maxDepthRecursive(root.right);
+        val queue: Queue<TreeNode?> = LinkedList();
 
-        var maxChildDepth = max(leftChildMaxDepth, rightChildMaxDepth);
+        queue.add(root);
 
-        return maxChildDepth + 1;
+        var currentDepth = 0;
+
+        var nodesInCurrentLevel = 1;
+
+        while (queue.isNotEmpty())
+        {
+            currentDepth++;
+
+            var nodesInNextLevel = 0;
+
+            if (nodesInCurrentLevel <= 0)
+            {
+                throw RuntimeException("queue is not empty, but we counted nodes in this level still!");
+            }
+
+            for (i in 0..nodesInCurrentLevel - 1)
+            {
+                var node = queue.remove();
+
+                if (node?.left != null)
+                {
+                    queue.add(node.left);
+
+                    nodesInNextLevel++;
+                }
+
+                if (node?.right != null)
+                {
+                    queue.add(node.right);
+
+                    nodesInNextLevel++;
+                }
+            }
+
+            nodesInCurrentLevel = nodesInNextLevel;
+        }
+
+        return currentDepth;
     }
 
-    fun maxDepth(root: TreeNode?): Int {
+    fun deepestLeavesSum(root: TreeNode?): Int {
 
-        return _maxDepthRecursive(root);
+        if (root == null)
+        {
+            return 0;
+        }
+
+        val maxDepth = computeMaxDepth(root);
+
+        val queue: Queue<TreeNode?> = LinkedList();
+
+        queue.add(root);
+
+        var currentDepth = 0;
+
+        var nodesInCurrentLevel = 1;
+
+        var sum = 0;
+
+        while (queue.isNotEmpty())
+        {
+            currentDepth++;
+            var nodesInNextLevel = 0;
+
+            for (i in 0..nodesInCurrentLevel - 1)
+            {
+                var node = queue.remove();
+
+                if (currentDepth == maxDepth)
+                {
+                    sum += node?.`val` ?: 0;
+                }
+
+                if (node?.left != null)
+                {
+                    queue.add(node.left);
+                    nodesInNextLevel++;
+                }
+
+                if (node?.right != null)
+                {
+                    queue.add(node.right);
+                    nodesInNextLevel++;
+                }
+            }
+
+            nodesInCurrentLevel = nodesInNextLevel;
+        }
+
+        return sum;
     }
 }
 
