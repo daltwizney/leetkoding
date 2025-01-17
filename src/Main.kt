fe8ebc0e4727266493adfe1f12162fd3e09a06dc
danrae pray <danrae.pray@outlook.com>
2025-01-17 11:32:09 -0600

    moar leetkoding!

--- src/Main.kt
+++ src/Main.kt
@@ -1,122 +1,18 @@
 import com.wizneylabs.leetcoding.GridRenderer
 import com.wizneylabs.leetcoding.exercises.HashTableLeetcode
 import com.wizneylabs.leetcoding.MaxHeap
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import java.util.*
 import kotlin.math.max
 import kotlin.math.min
 
-class TreeNode(var `val` : Int) {
-    var left : TreeNode? = null;
-    var right : TreeNode? = null;
-}
+fun main() = runBlocking {
 
-class Solution {
-
-    fun computeMaxDepth(root: TreeNode?): Int {
-
-        if (root == null)
-        {
-            return 0;
-        }
-
-        val queue: Queue<TreeNode?> = LinkedList();
-
-        queue.add(root);
-
-        var currentDepth = 0;
-
-        var nodesInCurrentLevel = 1;
-
-        while (queue.isNotEmpty())
-        {
-            currentDepth++;
-
-            var nodesInNextLevel = 0;
-
-            if (nodesInCurrentLevel <= 0)
-            {
-                throw RuntimeException("queue is not empty, but we counted nodes in this level still!");
-            }
-
-            for (i in 0..nodesInCurrentLevel - 1)
-            {
-                var node = queue.remove();
-
-                if (node?.left != null)
-                {
-                    queue.add(node.left);
-
-                    nodesInNextLevel++;
-                }
-
-                if (node?.right != null)
-                {
-                    queue.add(node.right);
-
-                    nodesInNextLevel++;
-                }
-            }
-
-            nodesInCurrentLevel = nodesInNextLevel;
-        }
-
-        return currentDepth;
+    launch { // launch a new coroutine and continue
+        delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
+        println("World!") // print after delay
     }
-
-    fun deepestLeavesSum(root: TreeNode?): Int {
-
-        if (root == null)
-        {
-            return 0;
-        }
-
-        val maxDepth = computeMaxDepth(root);
-
-        val queue: Queue<TreeNode?> = LinkedList();
-
-        queue.add(root);
-
-        var currentDepth = 0;
-
-        var nodesInCurrentLevel = 1;
-
-        var sum = 0;
-
-        while (queue.isNotEmpty())
-        {
-            currentDepth++;
-            var nodesInNextLevel = 0;
-
-            for (i in 0..nodesInCurrentLevel - 1)
-            {
-                var node = queue.remove();
-
-                if (currentDepth == maxDepth)
-                {
-                    sum += node?.`val` ?: 0;
-                }
-
-                if (node?.left != null)
-                {
-                    queue.add(node.left);
-                    nodesInNextLevel++;
-                }
-
-                if (node?.right != null)
-                {
-                    queue.add(node.right);
-                    nodesInNextLevel++;
-                }
-            }
-
-            nodesInCurrentLevel = nodesInNextLevel;
-        }
-
-        return sum;
-    }
-}
-
-fun main() {
-
-    println("hello leetkoding!");
+    println("Hello") // main coroutine continues while a previous one is delayed
 }
---
+++ src/Solution.kt
@@ -0,0 +1,9 @@
+
+class TreeNode(var `val` : Int) {
+    var left : TreeNode? = null;
+    var right : TreeNode? = null;
+}
+
+class Solution {
+
+}
