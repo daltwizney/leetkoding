0bd489c3ef9038f4644a7bf48f223e3eac7fac6c
danrae pray <danrae.pray@outlook.com>
2025-01-25 23:18:05 -0600

    moar leetkoding!

--- src/Solution.kt
+++ src/Solution.kt
@@ -1,6 +1,4 @@
 import java.util.*
-import kotlin.collections.ArrayDeque
-import kotlin.math.min
 
 class TreeNode(var `val` : Int) {
     var left : TreeNode? = null;
@@ -9,45 +7,95 @@ class TreeNode(var `val` : Int) {
 
 class Solution {
 
-    fun insertIntoBST(root: TreeNode?, `val`: Int): TreeNode? {
+    private fun _validPathRecursive(root: Int, visited: HashSet<Int>, graph: HashMap<Int, LinkedList<Int>>, destination: Int): Boolean {
 
-        if (root == null)
+        if (root == destination)
         {
-            return TreeNode(`val`);
+            return true;
         }
 
-        var currentNode = root;
+        if (visited.contains(root))
+        {
+            return false;
+        }
 
-        while (true) {
+        visited.add(root);
 
-            if (`val` < currentNode?.`val`!!)
+        val hasChildren = graph.containsKey(root);
+
+        var children: LinkedList<Int>? = null;
+
+        if (!hasChildren)
+        {
+            return false;
+        }
+
+        children = graph[root];
+
+        if (children!!.size == 0)
+        {
+            return false;
+        }
+
+        for (i in 0..children!!.size - 1) {
+
+            val child = children[i];
+
+            if (!visited.contains(child))
             {
-                if (currentNode.left == null)
-                {
-                    // add to tree
-                    currentNode.left = TreeNode(`val`);
-                    return root;
-                }
-                else
-                {
-                    currentNode = currentNode.left;
-                    continue;
-                }
-            }
-            else // `val` > currentNode?.`val`
-            {
-                if (currentNode.right == null)
-                {
-                    // add to tree
-                    currentNode.right = TreeNode(`val`)
-                    return root;
-                }
-                else
-                {
-                    currentNode = currentNode.right;
-                    continue;
+                val foundPath = _validPathRecursive(child, visited, graph, destination);
+
+                if (foundPath) {
+                    return true;
                 }
             }
         }
+
+        return false;
+    }
+
+    fun validPath(n: Int, edges: Array<IntArray>, source: Int, destination: Int): Boolean {
+
+        if (n == 0)
+        {
+            return false;
+        }
+        else if (source == destination)
+        {
+            return true;
+        }
+
+        // build adjacency list
+        val graph = hashMapOf<Int, LinkedList<Int>>();
+
+        for (i in 0..edges.size - 1)
+        {
+            val edge = edges[i];
+
+            val a = edge[0];
+            val b = edge[1];
+
+            if (!graph.containsKey(a))
+            {
+                graph[a] = LinkedList<Int>();
+            }
+
+            if (!graph.containsKey(b))
+            {
+                graph[b] = LinkedList<Int>();
+            }
+
+            graph[a]!!.add(b);
+            graph[b]!!.add(a);
+        }
+
+        if (!graph.containsKey(source) || !graph.containsKey(destination))
+        {
+            return false;
+        }
+
+        // do DFS search to check if path exists
+        val visited = hashSetOf<Int>();
+        return _validPathRecursive(source, visited, graph, destination);
     }
 }
