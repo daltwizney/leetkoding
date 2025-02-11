3bc46d780dda408df113154aec229cdfc92aea4d
danrae pray <danrae.pray@outlook.com>
2025-02-11 00:14:31 -0600

    moar leetkoding!

--- src/Solution.kt
+++ src/Solution.kt
@@ -1,4 +1,6 @@
+import kotlin.math.max
 import kotlin.math.min
+import kotlin.math.pow
 
 class TreeNode(var `val` : Int) {
     var left : TreeNode? = null;
@@ -11,56 +13,83 @@ class ListNode(var `val`: Int) {
 
 class Solution {
 
-    fun minDepth(root: TreeNode?): Int {
+    fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
 
-        if (root == null)
+        // compute the lengths of each list
+        var length1 = 0;
+        var length2 = 0;
+
+        var currentNode = l1;
+
+        while (currentNode != null)
         {
-            return 0;
+            length1++;
+            currentNode = currentNode.next;
         }
 
-        val queue = ArrayDeque<TreeNode>();
+        currentNode = l2;
 
-        queue.addLast(root);
-
-        var currentDepth = 1;
-        var nodesInCurrentDepth = 1;
-        var nodesInNextLevel = 0;
-
-        var minDepth = Int.MAX_VALUE;
-
-        while (queue.isNotEmpty())
+        while (currentNode != null)
         {
-            for (i in 0..nodesInCurrentDepth - 1)
+            length2++;
+            currentNode = currentNode.next;
+        }
+
+        // compute the sum using the lengths to understand the place value
+        var head: ListNode? = ListNode(0);
+
+        val maxLength = max(length1, length2);
+
+        var l1Node = l1;
+        var l2Node = l2;
+
+        currentNode = head;
+
+        var carryOne = false;
+
+        for (i in 0..maxLength - 1)
+        {
+            var value1 = 0;
+            var value2 = 0;
+
+            if (i < length1)
             {
-                val node = queue.removeFirst();
-
-                if (node.left != null)
-                {
-                    queue.addLast(node.left!!);
-                    nodesInNextLevel++;
-                }
-
-                if (node.right != null)
-                {
-                    queue.addLast(node.right!!);
-                    nodesInNextLevel++;
-                }
-
-                if (node.left == null && node.right == null)
-                {
-                    // it's a leaf node
-                    if (currentDepth < minDepth)
-                    {
-                        minDepth = currentDepth;
-                    }
-                }
+                value1 = l1Node!!.`val`;
+                l1Node = l1Node.next;
             }
 
-            nodesInCurrentDepth = nodesInNextLevel;
-            nodesInNextLevel = 0;
-            currentDepth++;
+            if (i < length2)
+            {
+                value2 = l2Node!!.`val`;
+                l2Node = l2Node.next;
+            }
+
+            var sum = value1 + value2;
+
+            if (carryOne)
+            {
+                sum += 1;
+            }
+
+            carryOne = if (sum / 10 == 1) true else false;
+            val remainder = sum % 10;
+
+            if (i == 0)
+            {
+                currentNode!!.`val` = remainder;
+            }
+            else
+            {
+                currentNode!!.next = ListNode(remainder);
+                currentNode = currentNode.next;
+            }
         }
 
-        return minDepth;
+        if (carryOne)
+        {
+            currentNode!!.next = ListNode(1);
+        }
+
+        return head;
     }
 }
