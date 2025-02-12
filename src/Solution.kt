1b6451252c077c9f36316789ac19b3d53e8c6c0f
danrae pray <danrae.pray@outlook.com>
2025-02-11 23:55:28 -0600

    moar leetkoding!

--- src/Solution.kt
+++ src/Solution.kt
@@ -13,83 +13,13 @@ class ListNode(var `val`: Int) {
 
 class Solution {
 
-    fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
+    fun reverseString(s: CharArray): Unit {
 
-        // compute the lengths of each list
-        var length1 = 0;
-        var length2 = 0;
-
-        var currentNode = l1;
-
-        while (currentNode != null)
+        for (i in 0..(s.size / 2) - 1)
         {
-            length1++;
-            currentNode = currentNode.next;
+            val temp = s[i];
+            s[i] = s[s.size - 1 - i];
+            s[s.size - 1 - i] = temp;
         }
-
-        currentNode = l2;
-
-        while (currentNode != null)
-        {
-            length2++;
-            currentNode = currentNode.next;
-        }
-
-        // compute the sum using the lengths to understand the place value
-        var head: ListNode? = ListNode(0);
-
-        val maxLength = max(length1, length2);
-
-        var l1Node = l1;
-        var l2Node = l2;
-
-        currentNode = head;
-
-        var carryOne = false;
-
-        for (i in 0..maxLength - 1)
-        {
-            var value1 = 0;
-            var value2 = 0;
-
-            if (i < length1)
-            {
-                value1 = l1Node!!.`val`;
-                l1Node = l1Node.next;
-            }
-
-            if (i < length2)
-            {
-                value2 = l2Node!!.`val`;
-                l2Node = l2Node.next;
-            }
-
-            var sum = value1 + value2;
-
-            if (carryOne)
-            {
-                sum += 1;
-            }
-
-            carryOne = if (sum / 10 == 1) true else false;
-            val remainder = sum % 10;
-
-            if (i == 0)
-            {
-                currentNode!!.`val` = remainder;
-            }
-            else
-            {
-                currentNode!!.next = ListNode(remainder);
-                currentNode = currentNode.next;
-            }
-        }
-
-        if (carryOne)
-        {
-            currentNode!!.next = ListNode(1);
-        }
-
-        return head;
     }
 }
